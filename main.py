bff96999bb63299aae49eea1a905c0828923e66a
IZFAISSAL <faissal.izermine@eleves.enpc.fr>
2023-09-08 16:41:52 +0200

    maniere d importer ask pdf

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-
-
-from ask_question_to_pdf import *
+from ask_question_to_pdf import ask_question_to_pdf
 
 
 app = Flask(__name__)
